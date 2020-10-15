f268e0fc67c51119a2051a524a59d672ef6ee598
Michal W <hicherkmic11@gmail.com>
2020-10-15 03:04:54 +0200

    Updates

---
+++ precache-manifest.f15c19faab1ae705bde3edce2fcf127d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e741f288481e212d762666d3fcbb7536",
+    "url": "/fahp/index.html"
+  },
+  {
+    "revision": "0c00a0be2c59723bfbce",
+    "url": "/fahp/static/css/main.65fe9460.chunk.css"
+  },
+  {
+    "revision": "f0f6cafeaaa697aa500a",
+    "url": "/fahp/static/js/2.41af0dad.chunk.js"
+  },
+  {
+    "revision": "e4d980926fadc05888ff1db78303136f",
+    "url": "/fahp/static/js/2.41af0dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c00a0be2c59723bfbce",
+    "url": "/fahp/static/js/main.c3370327.chunk.js"
+  },
+  {
+    "revision": "df48fed8828aff414e3f",
+    "url": "/fahp/static/js/runtime-main.68005664.js"
+  }
+]);
